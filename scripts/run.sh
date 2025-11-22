3722dc2b72719548a858aa70ca4ca5a2d589a38a
Tim <tim.eilers@protonmail.com>
2025-11-22 12:13:26 +0100

    build: cluster scripts

---
+++ scripts/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=saliency-alignment
+#SBATCH --output=logs/%x_%j.out
+#SBATCH --error=logs/%x_%j.err
+#SBATCH --ntasks=1
+#SBATCH --tmp=64G
+#SBATCH --mem-per-cpu=64G
+#SBATCH --gpus-per-node=1
+#SBATCH --nodes=1
+#SBATCH --time=24:00:00
+#SBATCH --gres=gpumem:64g
+
+echo "Beginning finetuning at $(date)"
+
+source scripts/env.sh
+
+uv run -m finetune
