fix: gradient checkpointing with fsdp

finetune/finetune.py
@@ -48,6 +48,9 @@ def finetune(cfg: DictConfig):
 
     # Prepare model for training
     model.train()
+    model.gradient_checkpointing_enable(
+        gradient_checkpointing_kwargs={"use_reentrant": False}
+    )
     model.config.use_cache = False
 
     # Prepare loggers
@@ -77,8 +80,10 @@ def finetune(cfg: DictConfig):
             reduce_dtype=torch.bfloat16,
             buffer_dtype=torch.bfloat16,
         ),
+        use_orig_params=True,
+        sync_module_states=True,
         cpu_offload=False,
-        activation_checkpointing_policy=auto_wrap_policy,
+        # activation_checkpointing_policy=auto_wrap_policy,
         limit_all_gathers=True,
     )
 

finetune/lightning.py
@@ -39,6 +39,7 @@ class FineTuner(L.LightningModule):
     def training_step(self, batch: dict, batch_idx: int):
         img_starts = batch.pop("img_starts")
         gen_starts = batch.pop("gen_starts")
+        self.accum.reset()
 
         # Forward pass with saliency accumulation
         with sdpa_saliency(self.accum, img_starts, gen_starts, self.head_dim):
@@ -69,11 +70,11 @@ class FineTuner(L.LightningModule):
     def validation_step(self, batch: dict, batch_idx: int):
         img_starts = batch.pop("img_starts")
         gen_starts = batch.pop("gen_starts")
+        self.accum.reset()
 
         # Forward pass with saliency accumulation
         with sdpa_saliency(self.accum, img_starts, gen_starts, self.head_dim):
-            with torch.inference_mode():
-                outputs = self.model(**batch, return_dict=True)
+            outputs = self.model(**batch, return_dict=True)
         loss = outputs.loss
 
         # Calculate auxiliary loss