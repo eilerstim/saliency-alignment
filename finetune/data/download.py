77f6e68f03f272126c224ea1c408328b0b7230f2
Tim Eilers <tim.eilers@protonmail.com>
2026-01-05 21:04:31 +0100

    chore: moved download script

--- finetune/data/download.py
+++ finetune/data/download.py
@@ -537,7 +537,7 @@ def download_png(data_cfg: DictConfig):
 
 @hydra.main(
     version_base="1.3",
-    config_path="../../../configs/data/",
+    config_path="../../configs/data/",
     config_name="png",
 )
 def main(cfg: DictConfig) -> None:
