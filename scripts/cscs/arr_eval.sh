6e3b8a621b1d35ae4112f095393b72c995278508
Tim Eilers <tim.eilers@protonmail.com>
2026-01-09 22:08:52 +0100

    fix: eval script

--- scripts/cscs/arr_eval.sh
+++ scripts/cscs/arr_eval.sh
@@ -22,10 +22,12 @@ MODEL_NAME="$(basename "${MODEL_PATH}")"
 echo "Starting LM-eval of ${MODEL_NAME} at $(date)"
 echo "MODEL_PATH=${MODEL_PATH}"
 
-MODEL_ARGS="pretrained=${MODEL_PATH} tokenizer=llava-hf/llava-1.5-7b-hf tensor_parallel_size=4 dtype=bfloat16 trust_remote_code=True max_model_len=32768"
-WANDB_ARGS="project=alignment-eval entity=teilers-eth-z-rich name=${MODEL_NAME} dir=${PROJECT_DIR}/outputs/lmms_eval/"
+MODEL_ARGS="pretrained=${MODEL_PATH},tokenizer=llava-hf/llava-1.5-7b-hf,tensor_parallel_size=4,dtype=bfloat16,trust_remote_code=True,max_model_len=32768"
+WANDB_ARGS="project=alignment-eval,entity=teilers-eth-z-rich,name=${MODEL_NAME},dir=${PROJECT_DIR}/outputs/lmms_eval/"
 
-python -m lmms_eval \
+# pip install wandb==0.17.0
+
+python3 -m lmms_eval \
     --model vllm \
     --model_args "${MODEL_ARGS}" \
     --output_path "${PROJECT_DIR}/results/lm-eval/${MODEL_NAME}" \
