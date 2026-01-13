8c243d2c479abc6ce0b40799d29f940536bc231e
alexander-brady <bradya@student.ethz.ch>
2026-01-13 11:55:33 +0100

    feat: use uv for env

--- scripts/cscs/count/env.sh
+++ scripts/cscs/count/env.sh
@@ -4,25 +4,28 @@
 
 export COUNT_DIR="$PROJECT_DIR/../grounding-vlms"
 export RESET_ENV=${RESET_ENV:-0}
+export VENV_DIR="$COUNT_DIR/.count_venv"
 
 # If reset_env is set, delete existing virtual environment
-if [ "$RESET_ENV" == "1" ]; then
-    rm -rf "$COUNT_DIR/.count_venv"
+if [ "$RESET_ENV" = "1" ]; then
+    rm -rf "$VENV_DIR"
 fi
 
 # If venv does not exist, create it
-if [ ! -d "$COUNT_DIR/.count_venv" ]; then
+if [ ! -d "$VENV_DIR" ]; then
     RESET_ENV=1
-    python3.12 -m venv "$COUNT_DIR/.count_venv" --system-site-packages
-    echo "Environment created at $COUNT_DIR/.count_venv"
+    uv venv "$VENV_DIR" --python 3.12 --system-site-packages
+    echo "Environment created at $VENV_DIR"
 else
-    echo "Environment already exists at $COUNT_DIR/.count_venv"
+    echo "Environment already exists at $VENV_DIR"
 fi
 
 # Activate the virtual environment
-source "$COUNT_DIR/.count_venv/bin/activate"
-if [ "$RESET_ENV" == "1" ]; then
-    python -m pip install -e "${COUNT_DIR}" --no-build-isolation --quiet
+source "$VENV_DIR/bin/activate"
+
+# Install package if environment was reset or newly created
+if [ "$RESET_ENV" = "1" ]; then
+    uv pip install -e "$COUNT_DIR" --no-build-isolation --quiet
 fi
 
 echo "Environment ready at $COUNT_DIR/.count_venv"
